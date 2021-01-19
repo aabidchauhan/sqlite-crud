4a920dbea4d0b702a4c5cbcd10a2dcbfd3e7f8df
aabidchauhan <abidchouhan1998@gmail.com>
2021-01-19 15:54:42 +0530

    Form Added with validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqlite_crud/models/contact.dart';
 
 const yellowColor = Color(0xff486579);
 void main() {
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SQLite CRUD',
       theme: ThemeData(
         primaryColor: yellowColor,
@@ -32,11 +34,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Contact _contact = Contact();
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -52,10 +52,60 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _form(),
+            _list(),
+          ],
         ),
       ),
     );
   }
+
+  //form function
+  _form() => Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Full Name'),
+                onSaved: (val) => setState(() => _contact.name = val),
+                validator: (val) =>
+                    (val.length == 0 ? 'This field is required' : null),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Mobile'),
+                onSaved: (val) => setState(() => _contact.mobile = val),
+                validator: (val) =>
+                    (val.length < 10 ? 'Atleast 10 digits required' : null),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () => _onSubmit(),
+                  child: Text('Submit'),
+                  color: Colors.blueGrey,
+                  textColor: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+
+  //Button function
+  _onSubmit() {
+    var form = _formKey.currentState;
+    if (form.validate()) {
+      //validation call
+      form.save();
+      print(_contact.name);
+    }
+  }
+
+  //List function
+  _list() => Container();
 }
